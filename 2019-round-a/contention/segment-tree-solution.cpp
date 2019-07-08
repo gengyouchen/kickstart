d1151534c56c5463c5b2256853c779bd37f017fb
Geng-You Chen <gengyouchen@gmail.com>
2019-07-08 15:51:33 +0800

    [Kick Start 2019 Round A] Contention

--- 2019-round-a/contention/segment-tree-solution.cpp
+++ 2019-round-a/contention/segment-tree-solution.cpp
@@ -1,19 +1,17 @@
 #include <algorithm>
 #include <climits>
 #include <iostream>
-#include <queue>
+#include <set>
+#include <unordered_set>
 #include <utility>
 #include <vector>
-#include <unordered_set>
-#include <unordered_map>
-#include <set>
 
 using namespace std;
 
 class SegTree {
 private:
-	vector<int> T, lazy; /* sum */
-	vector<unordered_set<int>> reqs; /* max prefix sum */
+	vector<int> T, lazy; /* tracking min */
+	vector<unordered_set<int>> reqs; /* can hook requests */
 	void propagate(int x, int L, int R) {
 		if (lazy[x]) {
 			T[x] += lazy[x];
@@ -21,6 +19,15 @@ private:
 				lazy[x * 2] += lazy[x], lazy[x * 2 + 1] += lazy[x];
 			lazy[x] = 0;
 		}
+	}
+	int query(int i, int j, int x, int L, int R) {
+		propagate(x, L, R);
+		if (j < L || R < i) /* no cover */
+			return INT_MAX;
+		if (i <= L && R <= j) /* total cover */
+			return T[x];
+		const int mid = L + (R - L) / 2;
+		return min(query(i, j, x * 2, L, mid), query(i, j, x * 2 + 1, mid + 1, R));
 	}
 	void update(int i, int j, int delta, int x, int L, int R) {
 		propagate(x, L, R);
@@ -34,15 +41,6 @@ private:
 		update(i, j, delta, x * 2, L, mid);
 		update(i, j, delta, x * 2 + 1, mid + 1, R);
 		T[x] = min(T[x * 2], T[x * 2 + 1]);
-	}
-	int query(int i, int j, int x, int L, int R) {
-		propagate(x, L, R);
-		if (j < L || R < i) /* no cover */
-			return INT_MAX;
-		if (i <= L && R <= j) /* total cover */
-			return T[x];
-		const int mid = L + (R - L) / 2;
-		return min(query(i, j, x * 2, L, mid), query(i, j, x * 2 + 1, mid + 1, R));
 	}
 	void hook(int i, int j, int req, bool deleted, int x, int L, int R) {
 		if (j < L || R < i) /* no cover */
@@ -68,11 +66,9 @@ private:
 				ret = searchMin(x * 2 + 1, mid + 1, R);
 		}
 		if (L == R)
-			ret.second = L;
-		if (ret.first == -1 && !reqs[x].empty()) {
-			//cout << "Found min at L=" << L << ", R=" << R << ", where val=" << query(L, R, x, L, R) << endl;
-			ret.first = *reqs[x].begin();
-		}
+			ret.first = L;
+		if (ret.second == -1 && !reqs[x].empty())
+			ret.second = *reqs[x].begin();
 		return ret;
 	}
 public:
@@ -80,40 +76,41 @@ public:
 		int nLeaf = 1;
 		while (nLeaf < n)
 			nLeaf *= 2;
-		T.resize(nLeaf * 2);
-		lazy.resize(nLeaf * 2);
-		reqs.resize(nLeaf * 2);
+		T.resize(nLeaf * 2), lazy.resize(nLeaf * 2), reqs.resize(nLeaf * 2);
 		fill(T.begin() + nLeaf + n, T.end(), INT_MAX);
 		for (int x = nLeaf - 1; x > 0; --x)
 			T[x] = min(T[x * 2], T[x * 2 + 1]);
 	}
-	void update(int i, int j, int delta) { return update(i, j, delta, 1, 0, T.size() / 2 - 1); }
-	int query(int i, int j) { return query(i, j, 1, 0, T.size() / 2 - 1); }
-	int get(int i) { return query(i, i); }
-	void set(int i, int val) { update(i, i, val - get(i)); }
-	void hook(int i, int j, int req, bool deleted) { return hook(i, j, req, deleted, 1, 0, T.size() / 2 - 1); }
-	int query() { return T[1]; }
-	pair<int, int> searchMin() { return searchMin(1, 0, T.size() / 2 - 1); }
+	void update(int i, int j, int delta) {
+		const int x = 1, L = 0, R = T.size() / 2 - 1;
+		return update(i, j, delta, x, L, R);
+	}
+	int query(int i, int j) {
+		const int x = 1, L = 0, R = T.size() / 2 - 1;
+		return query(i, j, x, L, R);
+	}
+	void set(int i, int val) {
+		update(i, i, val - query(i, i));
+	}
+	void hook(int i, int j, int req) {
+		const int x = 1, L = 0, R = T.size() / 2 - 1;
+		hook(i, j, req, false, x, L, R);
+	}
+	void unhook(int i, int j, int req) {
+		const int x = 1, L = 0, R = T.size() / 2 - 1;
+		hook(i, j, req, true, x, L, R);
+	}
+	pair<int, int> searchMin() {
+		const int x = 1, L = 0, R = T.size() / 2 - 1;
+		return searchMin(x, L, R);
+	}
 };
 
 class Solution {
 private:
-	template <class T, class F>
-	T lowestTrue(T low, T high, F cond) {
-		while (low < high) {
-			const T mid = low + (high - low) / 2;
-			if (cond(mid))
-				high = mid;
-			else
-				low = mid + 1;
-		}
-		return low;
-	}
-public:
-	int contention(int n, vector<pair<int, int>>& bookings) {
+	vector<pair<int, int>> getIdles(int n, vector<pair<int, int>>& bookings) {
 		vector<pair<int, int>> idles;
 		int idleBegin = 1;
-		sort(bookings.begin(), bookings.end());
 		for (const auto& booking : bookings) {
 			if (booking.first > idleBegin)
 				idles.emplace_back(idleBegin, booking.first - 1);
@@ -121,7 +118,9 @@ public:
 		}
 		if (idleBegin <= n)
 			idles.emplace_back(idleBegin, n);
-
+		return idles;
+	}
+	vector<int> getEvents(const vector<pair<int, int>>& bookings, const vector<pair<int, int>>& idles) {
 		unordered_set<int> events;
 		for (const auto& booking : bookings)
 			events.insert(booking.first), events.insert(booking.second + 1);
@@ -129,67 +128,45 @@ public:
 			events.insert(idle.first), events.insert(idle.second + 1);
 		vector<int> sortedEvents(events.begin(), events.end());
 		sort(sortedEvents.begin(), sortedEvents.end());
-		sortedEvents.pop_back();
-#if 0
-		cout << "sortedEvents: ";
-		for (int i : sortedEvents)
-			cout << i << ", ";
-		cout << endl;
-#endif
+		return sortedEvents;
+	}
+	int findEvent(int seat, const vector<int>& events) {
+		const auto it = upper_bound(events.begin(), events.end(), seat) - 1;
+		return distance(events.begin(), it);
+	}
+public:
+	/* time: O(q*log(q)), space: O(q), where q = # of bookings */
+	int contention(int n, vector<pair<int, int>>& bookings) {
+		sort(bookings.begin(), bookings.end());
+		const auto idles = getIdles(n, bookings);
+		const auto events = getEvents(bookings, idles);
+		const int q = bookings.size(), k = events.size() - 1;
 
-		SegTree s(sortedEvents.size());
-
+		SegTree s(k);
+		vector<int> gain(q);
+		set<pair<int, int>> candidates;
 		for (const auto& idle : idles) {
-			auto it1 = upper_bound(sortedEvents.begin(), sortedEvents.end(), idle.first) - 1;
-			auto it2 = upper_bound(sortedEvents.begin(), sortedEvents.end(), idle.second) - 1;
-			const int L = distance(sortedEvents.begin(), it1);
-			const int R = distance(sortedEvents.begin(), it2);
-			//cout << "idle L=" << idle.first << ", R=" << idle.second << ". L=" << L << ", R=" << R << endl;
+			const int L = findEvent(idle.first, events), R = findEvent(idle.second, events);
 			s.update(L, R, INT_MAX);
 		}
-
-		const int q = bookings.size();
-		vector<int> gain(q);
-		set<pair<int, int>> unbook;
-		for (int i = 0; i < q; ++i) {
-			auto it1 = upper_bound(sortedEvents.begin(), sortedEvents.end(), bookings[i].first) - 1;
-			auto it2 = upper_bound(sortedEvents.begin(), sortedEvents.end(), bookings[i].second) - 1;
-			const int L = distance(sortedEvents.begin(), it1);
-			const int R = distance(sortedEvents.begin(), it2);
-			//cout << "add L=" << L << ", R=" << R << endl;
-			s.hook(L, R, i, false);
-			s.update(L, R, 1);
-			unbook.emplace(gain[i], i);
+		for (int req = 0; req < q; ++req) {
+			const int L = findEvent(bookings[req].first, events), R = findEvent(bookings[req].second, events);
+			s.update(L, R, 1), s.hook(L, R, req), candidates.emplace(gain[req], req);
 		}
-		sortedEvents.push_back(n + 1);
+
 		int ans = INT_MAX;
-		while (!unbook.empty()) {
-			while (s.query() == 1) {
-				auto ret = s.searchMin();
-				int req = ret.first, pos = ret.second;
-				int w = sortedEvents[pos + 1] - sortedEvents[pos];
-
-				//cout << "searchMin: req=" << req << ", pos=" << pos << ", w=" << w << endl;
-				unbook.erase({gain[req], req});
-				gain[req] += w;
-				s.set(pos, INT_MAX);
-
-				unbook.emplace(gain[req], req);
+		while (!candidates.empty()) {
+			while (s.query(0, k - 1) == 1) {
+				const auto victim = s.searchMin();
+				const int i = victim.first, nSeats = events[i + 1] - events[i], req = victim.second;
+				candidates.erase({gain[req], req}), gain[req] += nSeats, candidates.emplace(gain[req], req);
+				s.set(i, INT_MAX);
 			}
-			int req = unbook.rbegin()->second;
-			unbook.erase({gain[req], req});
-			//cout << "extrat: req=" << req << ", gain=" << gain[req] << endl;
+			const int req = candidates.rbegin()->second;
 			ans = min(ans, gain[req]);
 
-			auto it1 = upper_bound(sortedEvents.begin(), sortedEvents.end(), bookings[req].first) - 1;
-			auto it2 = upper_bound(sortedEvents.begin(), sortedEvents.end(), bookings[req].second) - 1;
-			const int L = distance(sortedEvents.begin(), it1);
-			const int R = distance(sortedEvents.begin(), it2);
-			//cout << "add L=" << L << ", R=" << R << endl;
-			s.hook(L, R, req, true);
-			s.update(L, R, -1);
-
-
+			const int L = findEvent(bookings[req].first, events), R = findEvent(bookings[req].second, events);
+			s.update(L, R, -1), s.unhook(L, R, req), candidates.erase({gain[req], req});
 		}
 		return ans;
 	}
