700f44a3c09436e16fa3b2e33d355a2324d21a0d
Geng-You Chen <gengyouchen@gmail.com>
2019-08-05 14:17:48 +0800

    [Kick Start 2018 Round A] Even Digits

--- 2018-round-a/even-digits/greedy-solution.cpp
+++ 2018-round-a/even-digits/greedy-solution.cpp
@@ -1,28 +1,16 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
-#include <vector>
+#include <string>
 
 using namespace std;
 
 class Solution {
-private:
-	vector<int> num2digits(long long num) {
-		vector<int> digits;
-		while (num)
-			digits.push_back(num % 10), num /= 10;
-		return digits;
-	}
-	long long digits2num(const vector<int>& digits) {
-		long long num = 0;
-		for (int i = digits.size() - 1; i >= 0; --i)
-			num *= 10, num += digits[i];
-		return num;
-	}
 public:
 	/* time: O(log(n)), space: O(log(n)) */
 	long long evenDigits(long long n) {
-		const auto N = num2digits(n);
+		auto N = to_string(n);
+		reverse(N.begin(), N.end());
 
 		const auto it = find_if(N.rbegin(), N.rend(), [](int d) { return d % 2; });
 		if (it == N.rend())
@@ -30,25 +18,25 @@ public:
 		const int oddMSB = distance(it, N.rend()) - 1;
 
 		auto L = N;
-		--L[oddMSB], fill_n(L.begin(), oddMSB, 8);
+		--L[oddMSB], fill_n(L.begin(), oddMSB, '8'), reverse(L.begin(), L.end());
 
 		auto H = N;
 		bool carry = true;
 		for (int i = oddMSB; carry; ++i) {
 			if (i == H.size())
-				H.push_back(0);
+				H.push_back('0');
 
 			if (i == oddMSB)
 				++H[i];
 			else
 				H[i] += 2;
-			carry = (H[i] == 10);
+			carry = (H[i] > '9');
 			if (carry)
-				H[i] = 0;
+				H[i] = '0';
 		}
-		fill_n(H.begin(), oddMSB, 0);
+		fill_n(H.begin(), oddMSB, '0'), reverse(H.begin(), H.end());
 
-		return min(digits2num(H) - n, n - digits2num(L));
+		return min(stoll(H) - n, n - stoll(L));
 	}
 };
 
